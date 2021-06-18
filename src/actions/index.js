008f306a38663f6ae2820f1c4a357a7732e84cb9
Abhinav <pandeysandeep1190@gmail.com>
2021-06-18 20:34:38 +0530

    c43

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function singIN(data){
 
-    const request = axios.post('/usersignup',data)
+    const request = axios.post('https://loginback1.herokuapp.com/usersignup',data)
     .then((response)=>{
        return response.data
     })
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/', 
-        { target: 'https://login1x.herokuapp.com/' }
+        { target: 'https://loginback1.herokuapp.com//' }
     ));
 }
