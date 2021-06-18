4017c702077aac948bb675df63b7fafebdc44742
Abhinav <pandeysandeep1190@gmail.com>
2021-06-18 20:16:48 +0530

    c1

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function singIN(data){
 
-    const request = axios.post('/usersignup',data)
+    const request = axios.post('https://login1x.herokuapp.com/usersignup',data)
     .then(response=>response.data)
 
 
--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -2,6 +2,10 @@ export default function foo(state={},action){
 
 switch(action.type){
 
+
+case "sign_in":
+    return {...state,userDATA: action.payload}
+
 default:
     return state;
 }
