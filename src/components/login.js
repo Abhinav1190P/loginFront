1abeb7c2ed36ca2083820fc4d0e5b36106cc1469
Abhinav <pandeysandeep1190@gmail.com>
2021-06-19 20:58:52 +0530

    afafafaf

--- src/components/login.js
+++ src/components/login.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import * as actions from '../actions/index'
-import Home from './home'
+
 
 class Login extends Component {
 
