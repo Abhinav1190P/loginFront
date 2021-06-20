143d71a7ccf3e8219eaa6527598849ad1464f69b
Abhinav <pandeysandeep1190@gmail.com>
2021-06-20 15:17:45 +0530

    s2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Route, BrowserRouter} from 'react-router-dom'
 import signup from './components/signup'
 import Login from './components/login'
 import View from './components/userview'
-import Auth from './components/auth'
+
 import Home from './components/home'
 const App=()=> {
   return (
