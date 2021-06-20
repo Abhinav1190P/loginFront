89b689a97fea426025ae590d3ba31f4a1732c031
Abhinav <pandeysandeep1190@gmail.com>
2021-06-20 17:41:28 +0530

    push

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import {connect} from 'react-redux'
 import * as actions from '../actions/index'
-import { Redirect } from 'react-router-dom'
+
 
 
 class signup extends Component {
