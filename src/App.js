15d86e24b7041a0b49e07be2a56eeef4c7b0e687
Abhinav <pandeysandeep1190@gmail.com>
2021-06-20 17:32:59 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, BrowserRouter} from 'react-router-dom'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 import signup from './components/signup'
 import Login from './components/login'
 import View from './components/userview'
@@ -9,10 +9,12 @@ const App=()=> {
   return (
     <div>
 <BrowserRouter>
+<Switch>
 <Route path = "/" exact component = {Home}/>
 <Route path = "/sign" exact component = {signup}/>
 <Route path = "/login" exact component = {Login}/>
 <Route path = "/userview" exact component = {View}/>
+</Switch>
 </BrowserRouter>
 
     </div>
